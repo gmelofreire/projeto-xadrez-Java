78a6be18ce6b156317bf382aa633c3dcd851325d
Gustavo Freire <freiremelo14@gmail.com>
2023-10-26 08:59:21 -0300

    Classe Posicao implementada

---
+++ xadrez/src/model/application/Programa.java
@@ -0,0 +1,15 @@
+package model.application;
+
+import model.entities.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 7);
+		
+		System.out.println(pos);
+
+	}
+
+}
